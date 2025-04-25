253250cefac5c693dd5ffe97c937d36be045b81d
V-Karthik-M <karthikm2207@gmail.com>
2025-04-25 19:13:05 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
@@ -18,6 +18,9 @@ export default defineConfig(({ mode }) => {
     },
     build: {
       outDir: 'dist',
+    },
+    define: {
+      'process.env': env,
     },
   };
 });
