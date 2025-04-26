fba9e687941d7f2bbe1d399e4261fa3c35d0ac3f
V-Karthik-M <karthikm2207@gmail.com>
2025-04-25 23:00:05 -0400

    Update axiosConfig.js

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,9 +1,6 @@
-// src/axiosConfig.js
 import axios from "axios";
 
-const backendURL = import.meta.env.VITE_BACKEND_URL || "https://eventease-backend-j1ob.onrender.com/api"; 
-// ✅ Note the `/api` at end for correct routing
-
+const backendURL = import.meta.env.VITE_BACKEND_URL || "https://eventease-backend-j1ob.onrender.com";
 axios.defaults.baseURL = backendURL;
 axios.defaults.withCredentials = true;
 
