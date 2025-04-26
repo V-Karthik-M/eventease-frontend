1480a2733f48b96efcaf77054560888a4e979030
V-Karthik-M <karthikm2207@gmail.com>
2025-04-25 22:08:55 -0400

    Update axiosConfig.js

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,7 @@
 // src/axiosConfig.js
 import axios from "axios";
 
-const backendURL = import.meta.env.VITE_BACKEND_URL || "https://eventease-backend.onrender.com"; // ← your Render backend URL
+const backendURL = import.meta.env.VITE_BACKEND_URL || "https://eventease-backend-j1ob.onrender.com"; // ← your Render backend URL
 axios.defaults.baseURL = backendURL;
 axios.defaults.withCredentials = true;
 
